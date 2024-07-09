1ab847be99fdd78f2f664e051a8c241e653f4459
TohirL7 <007dastur007@gmail.com>
2024-07-09 20:05:11 +0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,40 @@
+// shrink
+window.addEventListener("scroll", function () {
+    shrink();
+  });
+  
+  let navbar = document.getElementById("navbar");
+  
+  function shrink() {
+    if (scrollY > 100) {
+      navbar.classList.add("navbar-shrink");
+    } else {
+      navbar.classList.remove("navbar-shrink");
+    }
+  }
+  
+  // navbar
+  document.addEventListener("DOMContentLoaded", () => {
+    const menuToggle = document.getElementById("menu-toggleIconId");
+    const navMenuId = document.getElementById("nav-menuId");
+    const navBtnId = document.getElementById("nav-btnId");
+  
+    console.log(menuToggle);
+    menuToggle.addEventListener("click", () => {
+      navMenuId.classList.toggle("active");
+      navBtnId.classList.toggle("activeBtn");
+    });
+  });
+  
+  let modeBtn = document.getElementById("mode-btn");
+  modeBtn.addEventListener("click", function () {
+      if (document.body.className !="dark"){
+              this.firstElementChild.src = " images/header/sun.svg";
+      }
+      else{
+          this.firstElementChild.src = "images/header/moon.svg";
+  } 
+  document.body.classList.toggle("dark");
+  
+  }
+  );
